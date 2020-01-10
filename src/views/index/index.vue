8e468634a21e8973fa957bb2e380e5636e8d3285
1357961823@qq.com <1357961823@qq.com>
2020-01-10 10:48:15 +0800

    备案号链接

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="main"> 首页
-      <a href="http://www.beian.miit.gov.cn/" style="margin-top: 100px">渝ICP备19013125号</a>
+      <a href="http://www.beian.miit.gov.cn/" target="_blank" style="margin-top: 100px;">渝ICP备19013125号</a>
     </div>
 
 </template>
